1014659e6fd0b7cc7394216ae1ac0cde0376ff43
koralle <mkhd7930@gmail.com>
2024-08-12 13:42:29 +0900

    feat(chibivue): implement first `createApp` function

---
+++ packages/chibivue/src/index.ts
@@ -0,0 +1,18 @@
+export interface Options {
+  render: () => string
+}
+
+export interface App {
+  mount: (selector: string) => void
+}
+
+export const createApp = (options: Options): App => {
+  return {
+    mount: (selector) => {
+      const root = document.querySelector(selector)
+      if (root) {
+        root.innerHTML = options.render()
+      }
+    },
+  }
+}
